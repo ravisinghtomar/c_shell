7082479ef4bd9e93803035cd6e7061a3f31da1b0
Prince Singh Tomar <singhtomarprince470@gmail.com>
2020-10-20 01:20:09 +0530

    issued fixed

--- echo.c
+++ echo.c
@@ -7,4 +7,43 @@ void execute_echo_func(char *command[200], int len, char command_string[200])
         printf("%s ", command[i]);
     }
     printf("\n");
+    
+    ///// Implementation End here   ------ 
+
+
+    ///// Below with more functionality of echo but removed after recieved mail about echo
+    /* 
+    if (!strcmp(command[1], "-n"))
+    {
+        int i = 0;
+        for (i = 0; i < strlen(com_str); i++)
+        {
+            if ((com_str[i] == '-') && (com_str[i + 1] == 'n'))
+            {
+                i += 2;
+                break;
+            }
+        }
+        for (; i < strlen(com_str); i++)
+        {
+            printf("%c", com_str[i]);
+        }
+    }
+    else
+    {
+        int i = 0;
+        for (i = 0; i < strlen(com_str); i++)
+        {
+            if ((com_str[i] == 'e') && (com_str[i + 1] == 'c'))
+            {
+                i += 5;
+                break;
+            }
+        }
+        for (; i < strlen(com_str); i++)
+        {
+            printf("%c", com_str[i]);
+        }
+        printf("\n");
+    } */ 
 }
